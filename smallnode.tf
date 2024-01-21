4841ad5fad9d069e7af9b23f223a23790e624b27
sgirdhari <sudesh@gatech.edu>
2024-01-21 17:35:32 -0500

    Delete smallnode.tf

--- smallnode.tf
+++
@@ -1,8 +0,0 @@
-module "linode" {
-  source           = "https://github.com/egeexyz/terraform-linode-instance.git"
-  authorized_keys  = [var.ssh_public_key]
-  type             = "g6-nanode-1"
-  region           = "us-central"
-  image            = "linode/ubuntu18.04"
-  root_pass        = "Terra4ormr0x!"
-}
