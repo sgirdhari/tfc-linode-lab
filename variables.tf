ed53d587e99bb978fcff4a807b4c53c1c5136375
sgirdhari <sudesh@gatech.edu>
2024-01-24 07:58:05 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,24 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+variable "linode_token" {
+  description = "Linode API Token"
+  type        = string
+  sensitive   = true
+}
+
 variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+  description = "Akamai region"
+  default     = "us-Central-1"
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  description = "Type of node instance to provision"
+  default     = "8gb Linode"
 }
 
 variable "instance_name" {
-  description = "EC2 instance name"
+  description = "Node instance name"
   default     = "Provisioned by Terraform"
 }
 
